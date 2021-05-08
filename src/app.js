b2677b387f190230184d62651178a78e566e5e9e
Raphiat89 <83375435+Raphiat89@users.noreply.github.com>
2021-05-07 22:44:45 -0600

    showing current weather condition elements

---
+++ src/app.js
@@ -0,0 +1,24 @@
+function displayTemperature (response){
+    console.log(response.data);
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.name;
+    let descriptionElement = document.querySelector("#description");
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML = response.data.main.humidity;
+    let windElement = document.querySelector("#wind");
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    
+
+}
+
+
+
+
+let apiKey = "62639e0b89c846487b043057770ede5f";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+
+axios.get(apiUrl).then(displayTemperature);
