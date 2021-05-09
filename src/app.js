20b952df193f63c28e413ec8fd3146c3e2c189e3
Raphiat89 <83375435+Raphiat89@users.noreply.github.com>
2021-05-08 22:30:48 -0600

    added the search engine

--- src/app.js
+++ src/app.js
@@ -37,10 +37,24 @@ function displayTemperature (response){
 }
 
 
-
-
+function search(city) {
 let apiKey = "62639e0b89c846487b043057770ede5f";
-let city = "Regina";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
 axios.get(apiUrl).then(displayTemperature);
+
+}
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+
+search("Regina");
+
+
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
