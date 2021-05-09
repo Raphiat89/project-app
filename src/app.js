9231f6a8d39b0af9688d9e0c1a5f7d1c5be103d4
Raphiat89 <83375435+Raphiat89@users.noreply.github.com>
2021-05-08 21:40:54 -0600

    added the relevant weather icon

--- src/app.js
+++ src/app.js
@@ -31,13 +31,16 @@ function displayTemperature (response){
     windElement.innerHTML = Math.round(response.data.wind.speed);
     let dateElement = document.querySelector("#date");
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    let iconElement = document.querySelector("#icon");
+    iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 
 
 
 let apiKey = "62639e0b89c846487b043057770ede5f";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
-
+let city = "Regina";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
