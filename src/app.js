ceae6de1347701454c4bfdc49737c24dce75b80b
Raphiat89 <83375435+Raphiat89@users.noreply.github.com>
2021-05-15 21:00:04 -0600

    Tried to worked on the forecast elements and data

--- src/app.js
+++ src/app.js
@@ -14,8 +14,48 @@ function formatDate(timestamp) {
 }
 
 
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "THu", "Fri", "Sat"];
+
+    return days[day];
+    
+}
+
 function displayForecast(response) {
-    console.log(response.data.daily);
+    let forecast = response.data.daily;
+
+    let forecastElement = document.querySelector("#forecast");
+
+    let forecastHTML = `<div class="row">`;
+    forecast.forEach(function(forecastDay, index) {
+        if (index < 6) {
+            forecastHTML = 
+            forecastHTML +
+            `
+                <div class="col-2">
+                    <div class="weather-forecast-date">${formatDay(forecastDay.dt)}
+                    </div>
+                    <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}10d@2x.png" 
+                    alt=""
+                    width="36"
+                    />
+                    <div class="weather-forecast-temperature">
+                        <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)}
+                         ᴼ
+                        </span>
+                        <span class="weather-forecast-temperature-min"> ${Math.round(forecastDay.temp.min)}
+                         ᴼ
+                        </span>
+                    </div>
+                </div>
+            `;
+        }
+    });
+
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
 }
 
 function getForecast(coordinates) {
